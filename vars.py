784c6a35e5e4e6a3a0da6748c6295aa6e5862b3f
1505-prinsu <patelkinjal2308@gmail.com>
2025-10-29 08:05:05 +0530

    Update API credentials and bot username

--- vars.py
+++ vars.py
@@ -1,5 +1,6 @@
-api_id = 22475741
-api_hash = "1a217be71a0225e0a678af286c211f8a"
-bot_token = "7086426763:AAFAbz6xw3u0F7EfQnpeZ0nmeOloLkpvKC0"
-AUTH = 6562642609
-#Bot Username = "bhuriyabhaig"
+api_id = 27433400
+api_hash = "1a286620de5ffe0a7d9b57e604293555"
+bot_token = "7086426763:""
+AUTH = 6201066540
+#Bot Username = "Nondrm1505uploder_bot"
+
